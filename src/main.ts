efad2be6e714b0a373142be72b12e75b2ab003e9
Aidan Juma <aidan@aidanjuma.dev>
2024-01-12 02:34:33 +0000

    ✨ renders .obj files now!

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
 import * as THREE from "three";
+import { OBJLoader } from "three/examples/jsm/loaders/OBJLoader.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
-// This is just a placeholder scene for now; used it to test ThreeJS functionality.
 const scene = new THREE.Scene();
+
+// Set up the camera:
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
@@ -9,24 +12,62 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+camera.position.z = 50;
+
+// Create the renderer:
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000, 1);
+
+// Append the renderer to the DOM. Prevent scrolling as well!
+document.body.style.margin = "0";
+document.body.style.overflow = "hidden";
 document.body.appendChild(renderer.domElement);
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+// Light the scene appropriately, i.e. makes model visible.
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
+topLight.castShadow = true;
+scene.add(topLight);
 
-camera.position.z = 5;
+const ambientLight = new THREE.AmbientLight(0x333333, 5);
+scene.add(ambientLight);
 
-const animate = () => {
+// Configure controls using OrbitControls:
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.minDistance = 10;
+controls.maxDistance = 500;
+controls.zoomSpeed = 0.5;
+
+// Load .obj model!
+const loader = new OBJLoader();
+loader.load("models/white-couch/86.obj", function (object) {
+  scene.add(object);
+
+  // Calculate the object's center,
+  const box = new THREE.Box3().setFromObject(object);
+  const center = box.getCenter(new THREE.Vector3());
+
+  // ...and then position the camera to face toward the object's center.
+  camera.position.set(center.x, center.y, center.z + 100);
+  camera.lookAt(center);
+
+  // Update controls as necessary!
+  controls.target.set(center.x, center.y, center.z);
+  controls.update();
+});
+
+function animate() {
   requestAnimationFrame(animate);
-
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-
+  controls.update();
   renderer.render(scene, camera);
-};
+}
 
 animate();
+
+window.addEventListener("resize", () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  controls.update();
+});
